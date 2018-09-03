d934f39ca8b747ff8f4c77587e1a5706e711762f
ElCondeDeMonteCrypto <42327281+ElCondeDeMonteCrypto@users.noreply.github.com>
2018-09-03 01:12:10 -0500

    Delete testContract.sol
    Making some changes, no point on keeping.

--- testContract.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.22;
-contract TestContract{
-    string constant messageKeeper = "I'm in the blockchain";
-    function getTestContract() public returns(string){
-        return messageKeeper;
-    }
-}
